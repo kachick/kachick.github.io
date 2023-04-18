f8216ab3f2097622512296ea93ab6a0ca48399be
Kenichi Kamiya <kachick1@gmail.com>
2023-04-19 03:34:21 +0900

    Add nix settings to vscode config

--- shell.nix
+++ shell.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4c3edba85629ec304b5269e4b0ac7f26c433df23.tar.gz") { } }:
+{ pkgs ? import
+    (fetchTarball
+      "https://github.com/NixOS/nixpkgs/archive/4c3edba85629ec304b5269e4b0ac7f26c433df23.tar.gz")
+    { }
+}:
 
 pkgs.mkShell {
   buildInputs = [
