bfb74ee3003dea95f29b5a9bb8af5db2ba1939d5
Kenichi Kamiya <kachick1@gmail.com>
2023-07-05 11:10:51 +0900

    Update deprecated deno API

--- scripts/isEmpty.ts
+++ scripts/isEmpty.ts
@@ -1,7 +1,5 @@
-import {
-  readAllSync,
-  writeAllSync,
-} from 'https://deno.land/std@0.140.0/streams/conversion.ts';
+import { readAllSync } from 'https://deno.land/std@0.193.0/streams/read_all.ts';
+import { writeAllSync } from 'https://deno.land/std@0.193.0/streams/write_all.ts';
 
 const input = readAllSync(Deno.stdin);
 
