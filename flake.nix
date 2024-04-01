2e2121b4782efb6c0eb9718ec670a0e93f5b89d1
Kenichi Kamiya <kachick1@gmail.com>
2024-04-01 13:42:33 +0900

    Import latest anylang-template

--- flake.nix
+++ flake.nix
@@ -5,28 +5,37 @@
     #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
     # How to update the revision
     #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    edge-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, edge-nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
+        edge-pkgs = edge-nixpkgs.legacyPackages.${system};
+      in {
+        # Q. Why nixfmt? Not nixpkgs-fmt and alejandra?
+        # A. nixfmt will be official
+        # - https://github.com/NixOS/nixfmt/issues/153
+        # - https://github.com/NixOS/nixfmt/issues/129
+        # - https://github.com/NixOS/rfcs/pull/166
+        formatter = edge-pkgs.nixfmt;
         devShells.default = with pkgs;
           mkShell {
             buildInputs = [
               # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+              # https://github.com/kachick/dotfiles/pull/228
               bashInteractive
-
-              deno
-              # expect # Enable if needed to keep color in tee of local development
               direnv
-              dprint
-              nil
-              nixpkgs-fmt
+              edge-pkgs.nixfmt
+              edge-pkgs.nil
+
+              edge-pkgs.deno
+              edge-pkgs.dprint
+              edge-pkgs.typos
+              edge-pkgs.go-task
             ];
           };
       });
